f6212a33f3d29bcf88f3067b430c7761929c0e32
aln730 <arnsg730@proton.me>
2025-05-27 01:41:17 -0400

    scaffolding

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,5 +19,19 @@ var (
         tunnelsMu sync.RWMutex
 )
 
+func handleRegister(){
 
+}
+
+func handleProxy(){
+
+}
+
+func main(){
+    http.HandleFunc("/register", handleRegister)
+    http.HandleFunc("/",handleProxy)
+
+    log.Println("Server listening on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
