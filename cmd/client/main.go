cad907150fa70208e09d5a06093b2e3b98560add
aln730 <arnsg730@proton.me>
2025-05-27 02:36:29 -0400

    i am stupid pt3 but it works

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,34 +1,42 @@
 package main
 
-import(
-    "flag"
-    "fmt"
-    "log"
-    "net/http"
-    "net/url"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 )
 
-func main(){
+func main() {
 
-    name := flag.String("name","test", "Subdomain name")
-    local := flag.String("local","http://localhost:3000/","Local target URL")
-    server := flag.String("server","http://localhost:8080/","Tunnel server URL")
-    flag.Parse()
+	name := flag.String("name", "test", "Subdomain name")
+	local := flag.String("local", "http://localhost:3000", "Local target URL")
+	server := flag.String("server", "http://localhost:8080", "Tunnel server URL")
+	flag.Parse()
 
-    registerURL := fmt.Sprintf("%s/register?name=%s&target=%s", *server, *name, *local)
-    resp, err := http.Get(registerURL)
-    if err != nil {
-        log.Fatalf("Failed to register tunnel: %v", err)
-    }
-    defer resp.Body.Close()
+	registerURL := fmt.Sprintf("%s/register?name=%s&target=%s", *server, *name, *local)
+	resp, err := http.Get(registerURL)
+	if err != nil {
+		log.Fatalf("Failed to register tunnel: %v", err)
+	}
+	defer resp.Body.Close()
 
-    log.Printf("Tunnel registered. Access via %s/%s\n", *server, *name)
+	log.Printf("Tunnel registered. Access via: %s/%s\n", *server, *name)
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *httpRequest){
-        fmt.Fprintln(w, "Greetings from the local server")
-    })
+	parsedURL, err := url.Parse(*local)
+	if err != nil {
+		log.Fatalf("Invalid local URL: %v", err)
+	}
 
-    parsedURL,_ := url.Parse(*local)
-    log.Printf("Local server running at %s\n", parsedURL.Host)
-    log.Fatal(http.ListenAndServe(parsedURL.Host, nil))
+	if parsedURL.Host == "" {
+		log.Fatalf("Please provide a valid host:port in the --local flag")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Greetings from %s!\nRequested Path: %s\n", parsedURL.Host, r.URL.Path)
+	})
+
+	log.Printf("Local server running at http://%s\n", parsedURL.Host)
+	log.Fatal(http.ListenAndServe(parsedURL.Host, nil))
 }
